ecee5cb0a9d6ce125d12184b456106509435f2c3
Sean Morrison <sean1972@gmail.com>
2018-07-14 11:43:55 -0700

    Used for...of loop to test that URLs are defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('each feed has a URL defined', function() {
+           for (var i in allFeeds) {
+             expect(allFeeds[i].url).toBeDefined();
+             expect(allFeeds[i].url.length).not.toBe(0);
+           };
+         });
 
 
         /* TODO: Write a test that loops through each feed
